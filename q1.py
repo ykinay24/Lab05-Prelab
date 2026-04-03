426dedec5b0c3b04b95c2b11766f608b75162f49
ykinay24 <ykinay24@ku.edu.tr>
2026-04-03 14:49:57 +0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -2,4 +2,25 @@ def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+
+    def _expand_around_center(left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1:right]
+
+    longest = ""
+
+    for i in range(len(s)):
+        odd_pal = _expand_around_center(i, i)
+        even_pal = _expand_around_center(i, i + 1)
+
+        if len(odd_pal) > len(longest):
+            longest = odd_pal
+        if len(even_pal) > len(longest):
+            longest = even_pal
+
+    if len(longest) <= 1:
+        return ""
+
+    return longest
